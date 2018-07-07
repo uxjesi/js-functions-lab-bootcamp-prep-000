c9d5474923b3c317f2f646004fd0433471d9d7cd
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 20:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+var name = "Jessica"
+
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (string) {
+  return `Happy Hoidays, ${name}!`
+}
+
