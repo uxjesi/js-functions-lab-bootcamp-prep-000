eae3768b7c5981172251e9f8cc59a6bdce772bb2
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 20:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function happyHolidaysTo (string) {
   return `Happy Hoidays, ${name}!`
 }
 
+function happyHolidayTo () {
+  return `Happy ${holiday}, ${name}!`
+}
+
+
+function holidayCountdown () {
+  return `It's ${days} days until ${holiday}!`
+}
