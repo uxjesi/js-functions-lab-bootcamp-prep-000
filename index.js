916cc877244a49048d3d0dc3fb4efb1981cd3206
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 21:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo (name) {
-  return `Happy Holidays, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo (holiday, name) {
